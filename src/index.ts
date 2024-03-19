8fc6652cf304b9adcb2da62bce3c4a09bbf1c232
laurourbano <74082012+laurourbano@users.noreply.github.com>
2024-03-18 21:19:55 -0300

    initial commit: 2024-03-18 21:19:00

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", router);
+
+app.listen(process.env.PORT || "3333", () => {
+  console.log(`Server is running on port ${process.env.PORT || "3333"}`);
+});
